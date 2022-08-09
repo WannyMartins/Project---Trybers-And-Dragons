de98a2aaf3b06569033e39b938cdfb61ad541f16
Wanny Martins <wcatery@gmail.com>
2022-08-09 18:44:08 -0300

    feat(Archetype): implementando class mage que herda de Archerype

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Mage extends Archetype {
+  _energyType: EnergyType;
+  private static instanceCount = 0;
+
+  constructor(name: string) {
+    super(name);
+    Mage.instanceCount += 1;
+    this._energyType = 'mana';
+  }
+
+  static createdArchetypeInstances(): number {
+    return Mage.instanceCount;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
+
+export default Mage;
