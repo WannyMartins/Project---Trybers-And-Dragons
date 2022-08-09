5dd8fb953eac9d922d5549c4ab5e6d0d013a00b0
Wanny Martins <wcatery@gmail.com>
2022-08-09 18:57:47 -0300

    feat(Archetype): criada classe extendida Warrior

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Warrior extends Archetype {
+  _energyType: EnergyType;
+  private static instanceCount = 0;
+
+  constructor(name: string) {
+    super(name);
+    Warrior.instanceCount += 1;
+    this._energyType = 'stamina';
+  }
+
+  static createdArchetypeInstances(): number {
+    return this.instanceCount;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
+
+export default Warrior;
