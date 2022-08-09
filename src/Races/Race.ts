849edd8d48640693b6f53cab221121ff38a71c77
Wanny Martins <wcatery@gmail.com>
2022-08-09 15:42:47 -0300

    feat(Race):

---
+++ src/Races/Race.ts
@@ -0,0 +1,30 @@
+import IRace from '../Interfaces/IRaces';
+
+abstract class Race implements IRace {
+  protected _name: string; 
+  private _dexterity: number; 
+
+  constructor(
+    name: string, 
+    dexterity: number, 
+  ) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static get createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
