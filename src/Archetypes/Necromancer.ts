dfaec504c1fdc381ec2e34e308b0b92d0ec96e6e
Wanny Martins <wcatery@gmail.com>
2022-08-09 18:55:26 -0300

    feat(Archetype): criando classe extendida Necromancer

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Necromancer extends Archetype {
+  _energyType: EnergyType;
+  private static instanceCount = 0;
+
+  constructor(name: string) {
+    super(name);
+    Necromancer.instanceCount += 1;
+    this._energyType = 'mana';
+  }
+
+  static createdArchetypeInstances(): number {
+    return this.instanceCount;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
+
+export default Necromancer;
